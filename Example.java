5ae033f0124b1f42d90b4bec50c7db76d5435078
dhanshree4 <52560881+dhanshree4@users.noreply.github.com>
2019-07-08 16:46:45 +0530

    uplodaded on GIT for jenkins linking
    we are going to upload and link java files with jenkins so in next update we will write all the steps for linking with jenkins

---
+++ Example.java
@@ -0,0 +1,15 @@
+public class Example {
+	
+	public static void main(String[] args)
+	{
+		String myName = "Akash" ;
+	    int myAge =80 ;
+	
+		System.out.println("Name is: " + myName + "Age is: " + myAge);
+		
+		return ;
+		
+	}
+}
+
+ 
